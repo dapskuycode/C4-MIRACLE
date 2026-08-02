d93121808b21677bfb16ee7bb468989255110b9a
daffa <dakerr19@gmail.com>
2026-08-02 20:46:57 +0700

    Initial Commit

---
+++ C4-MIRACLE/C4_MIRACLEApp.swift
@@ -0,0 +1,17 @@
+//
+//  C4_MIRACLEApp.swift
+//  C4-MIRACLE
+//
+//  Created by M Daffa Atstsaqif on 02/08/26.
+//
+
+import SwiftUI
+
+@main
+struct C4_MIRACLEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
